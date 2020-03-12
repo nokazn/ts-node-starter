38e3b461055674fb97115c6397e2dce7d4951a24
nokazn <41154684+nokazn@users.noreply.github.com>
2020-03-12 15:58:49 +0900

    update lint settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,26 @@
 module.exports = {
   "env": {
-      "es6": true,
-      "node": true
+    "es6": true,
+    "node": true
   },
   "extends": [
-      "plugin:@typescript-eslint/recommended",
-      "plugin:@typescript-eslint/eslint-recommended",
-      "airbnb-base"
+    "plugin:@typescript-eslint/eslint-recommended",
+    "airbnb-base",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/typescript",
   ],
   "globals": {
-      "Atomics": "readonly",
-      "SharedArrayBuffer": "readonly"
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
   },
   "parser": "@typescript-eslint/parser",
   "parserOptions": {
-      "ecmaVersion": 2018,
-      "sourceType": "module"
+    "ecmaVersion": 2018,
+    "sourceType": "module"
   },
   "plugins": [
-      "@typescript-eslint"
+    "@typescript-eslint"
   ],
   "rules": {
     "import/extensions": [2, {
@@ -26,5 +28,11 @@ module.exports = {
       "ts": "never",
       "json": "never"
     }],
+    "indent": [2, 2],
+    "lines-between-class-members": 0,
+    "no-console": 0,
+    // typescript-eslint の no-unuserd-vars を有効にする
+    "no-unused-vars": 0,
+    "@typescript-eslint/no-unused-vars": 2,
   }
 };
