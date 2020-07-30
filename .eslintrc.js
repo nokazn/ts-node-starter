1bbd7b2b4dea82b60ad2dc2ed4840caf24a932f7
nokazn <nokazn@gmail.com>
2020-07-30 23:15:39 +0900

    fix eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,38 +1,40 @@
 module.exports = {
-  "env": {
-    "es6": true,
-    "node": true
+  env: {
+    es6: true,
+    node: true,
   },
-  "extends": [
-    "plugin:@typescript-eslint/eslint-recommended",
-    "airbnb-base",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
+  extends: [
+    'plugin:@typescript-eslint/eslint-recommended',
+    'airbnb-base',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
   ],
-  "globals": {
-    "Atomics": "readonly",
-    "SharedArrayBuffer": "readonly"
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
-  "parser": "@typescript-eslint/parser",
-  "parserOptions": {
-    "ecmaVersion": 2018,
-    "sourceType": "module"
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
   },
-  "plugins": [
-    "@typescript-eslint"
+  plugins: [
+    '@typescript-eslint',
   ],
-  "rules": {
-    "import/extensions": [2, {
-      "js": "never",
-      "ts": "never",
-      "json": "never"
+  rules: {
+    'import/extensions': [2, {
+      ts: 'never',
+      tsx: 'never',
+      js: 'never',
+      jsx: 'never',
+      json: 'never',
     }],
-    "indent": [2, 2],
-    "lines-between-class-members": 0,
-    "no-console": 0,
+    indent: [2, 2],
+    'lines-between-class-members': 0,
+    'no-console': 0,
     // typescript-eslint の no-unuserd-vars を有効にする
-    "no-unused-vars": 0,
-    "@typescript-eslint/no-unused-vars": 2,
-  }
+    'no-unused-vars': 0,
+    '@typescript-eslint/no-unused-vars': 2,
+  },
 };
