d28a6d10553e4cbcbd46f229e5ed6a556de5c534
41154684+nokazn@users.noreply.github.com <41154684+nokazn@users.noreply.github.com>
2022-06-05 14:27:47 +0900

    deps: modify ts-jest to @swc/jest

--- jest.config.ts
+++ jest.config.ts
@@ -4,12 +4,7 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
   roots: ['.'],
   transform: {
-    '^.+\\.ts$': 'ts-jest',
-  },
-  globals: {
-    'ts-jest': {
-      tsconfig: './tsconfig.test.json',
-    },
+    '^.+\\.ts$': '@swc/jest',
   },
   moduleNameMapper: {
     '^~/(.*)$': path.resolve(__dirname, 'src/$1'),
